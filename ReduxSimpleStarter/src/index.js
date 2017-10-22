faf00f54ed8a191fe7974d05b3ed4a485fe93a9b
anupkv1 <anupkulkarni563@gmail.com>
2017-10-22 16:04:23 -0700

    vide list ready

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+//create a new component this component should produce
+// some html
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search'
+import VideoList from './components/video_list'
+const API_KEY = 'AIzaSyDFDhAHdH1icrs9itS0vJ0MtJ5ssHbTIy4';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class  App extends Component {
+
+  constructor(props) {
+      super(props);
+
+      this.state = { videos: [] };
+
+
+      YTSearch({key:API_KEY, term: 'surf boards'}, (videos) =>{
+        console.log(videos);
+        this.setState({
+          videos
+        });
+      });
+
+  }
+    render(){
+    return(
+      <div>
+      <SearchBar />
+      <VideoList />
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App/>, document.querySelector('.container'));
+
+
+// put the component's generated HTML and put it on
+// the page(into the DOM)
